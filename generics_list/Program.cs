1028f42aacc775604bf358f164a63c519c88831b
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-21 00:26:45 -0600

    Add project files.

---
+++ generics_list/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> s = new List<string>(7);
+            s.Add("Ilyas");
+            s.Add("Teja");
+            s.Add("Umakanth");
+            s.Add("Rajeesh");
+            s.Add("Rohan");
+            s.Add("Bharat");
+            s.Add("Venu");
+            foreach (var v  in s)
+                Console.WriteLine(v);
+            Console.ReadKey();
+        }
+    }
+}
